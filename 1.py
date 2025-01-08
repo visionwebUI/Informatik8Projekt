e3676745d187d96f74d3b926d39231a741aa4689
btfmedia <161230093+visionwebUI@users.noreply.github.com>
2025-01-08 11:22:21 +0100

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+import random
+
+print(f'Willkommen...') #Der User wird über das erfolgreiche abschließen des Codes abgeschlossen
+x = random.randint(2, 25) #X wird deffiniert
+print(x) #X wird in die Konsole Geschrieben
+y = random.randint(2, 25) #Y wird deffiniert
+print(y) #Y wird in die Konsole Geschrieben
+print(f'Code erfolgreich abgeschlossen. Bis zum nächsten mal!')
